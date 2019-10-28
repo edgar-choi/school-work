44cdb911ee625bf6dfa3875ab981c4c45781eeea
edgar-choi <55901462+edgar-choi@users.noreply.github.com>
2019-10-28 16:12:39 -0400

    add oop244 files

---
+++ ws1/at_home/tools.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef SICT_TOOLS_H
+#define SICT_TOOLS_H
+namespace sict {
+	// Displays the user interface menu
+	int menu();
+
+	// Performs a fool-proof integer entry
+	int getInt(int min, int max);
+}
+#endif
